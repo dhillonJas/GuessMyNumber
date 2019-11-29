4c115f7a4c704774d742438d167239e7eb48d14d
Jasdeep Singh <55818488+dhillonJas@users.noreply.github.com>
2019-11-29 09:38:27 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args) {
         Game test = new Game();
-        System.out.println("Imagine a number between 1 and 30.\nI will try to guess it, if you are not a lying bitch!!\nPress ENTER");
+        System.out.println("Imagine a number between 1 and 30.\nI will try to guess it. \nPress ENTER");
         Scanner sc = new Scanner(System.in);
         String l = sc.nextLine();
         test.number();
