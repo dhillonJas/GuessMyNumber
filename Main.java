56c4910d5a7da1e997c8063de9c14ab41fa6293b
Jasdeep Singh <55818488+dhillonJas@users.noreply.github.com>
2019-11-29 09:44:27 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,10 +4,15 @@ public class Main
 {
     public static void main(String[] args) {
         Game test = new Game();
-        System.out.println("Imagine a number between 1 and 30.\nI will try to guess it. \nPress ENTER");
+        System.out.println("Imagine a number between 1 and 30.\nI will try to guess it.\nPress ENTER to start, Q to quit.");
         Scanner sc = new Scanner(System.in);
-        String l = sc.nextLine();
-        test.number();
-
+        String start = sc.nextLine();
+        while(!start.equalsIgnoreCase("Q"))
+        {
+            test.number();
+            System.out.println("\nImagine a number between 1 and 30.\nI will try to guess it\nPress ENTER to start, Q to quit.");
+            sc = new Scanner(System.in);
+            start = sc.nextLine();
+        }
     }
 }
